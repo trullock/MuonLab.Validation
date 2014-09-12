24b2c5ad1110a4d975c2860e4ecd3b35cb25b736
Harry Boyes <harry.boyes@myunidays.com>
2014-09-12 15:18:52 +0100

    Fixed null is equal to null comparison

---
+++ src/MuonLab.Validation.Tests/Enumerables/When_validating_no_duplicates.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace MuonLab.Validation.Tests.Enumerables
+{
+	public class When_validating_no_duplicates
+	{
+
+
+
+
+
+		public class TestClass
+		{
+			public IList<InnerClass> List { get; set; }
+		}
+
+		public class InnerClass
+		{
+			public string Name { get; set; }
+		}
+
+		public class TestClassValidator : Validator<TestClass>
+		{
+			private readonly InnerClassValidator innerClassValidator;
+
+			public TestClassValidator()
+			{
+				this.innerClassValidator = new InnerClassValidator();
+			}
+
+			protected override void Rules()
+			{
+				Ensure(x => x.List.AllSatisfy(innerClassValidator));
+			}
+		}
+
+		public class InnerClassValidator : Validator<InnerClass>
+		{
+			protected override void Rules()
+			{
+				Ensure(x => x.Name.IsNotNullOrEmpty());
+			}
+		}
+
+	}
+}
