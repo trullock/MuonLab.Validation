9e16294c3d2e41c4f5ba8ce16e4e7801f58fbbb7
Andrew Bullock <trullock@gmail.com>
2018-07-13 09:55:15 +0100

    added async satisfied syntax

---
+++ src/MuonLab.Validation.Tests/EnsureAsync/When_validating_a_property_as_false.cs
@@ -0,0 +1,61 @@
+using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace MuonLab.Validation.Tests.EnsureAsync
+{
+	[TestFixture]
+	public class When_validating_a_property_as_false
+	{
+		private TestClassValidator validator;
+
+		[SetUp]
+		public void SetUp()
+		{
+			this.validator = new TestClassValidator();
+		}
+
+		[Test]
+		public void ensure_false_returns_true()
+		{
+			var testClass = new TestClass(false);
+
+			var validationReport = this.validator.Validate(testClass);
+
+			Assert.IsTrue(validationReport.IsValid);
+		}
+
+		[Test]
+		public void ensure_true_returns_false()
+		{
+			var testClass = new TestClass(true);
+
+			var validationReport = this.validator.Validate(testClass);
+
+			validationReport.Violations.First().Error.Key.ShouldEqual("BeFalse");
+		}
+
+		private class TestClass
+		{
+			public bool value { get; set; }
+
+			public TestClass(bool value)
+			{
+				this.value = value;
+			}
+		}
+
+		private class TestClassValidator : Validator<TestClass>
+		{
+			protected override void Rules()
+			{
+				Ensure(x => x.value.Satisfies(v => AsyncFalseCheck(v), "BeFalse"));
+			}
+
+            async Task<bool> AsyncFalseCheck(bool b)
+            {
+                return b == false;
+            }
+		}
+	}
+}
--- src/MuonLab.Validation/Extensions/SatisfiesExtension.cs
+++ src/MuonLab.Validation/Extensions/SatisfiesExtension.cs
@@ -1,15 +1,20 @@
 using System;
 using System.Collections.Generic;
-using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 // ReSharper disable once CheckNamespace
 namespace MuonLab.Validation
 {
 	public static class SatisfiesExtension
 	{
-		public static ICondition<TValue> Satisfies<TValue>(this TValue self, Expression<Func<TValue, bool>> condition, string errorKey)
+		public static ICondition<TValue> Satisfies<TValue>(this TValue self, Func<TValue, bool> condition, string errorKey)
 		{
-			return new PropertyCondition<TValue>(condition.Compile(), errorKey);
+			return new PropertyCondition<TValue>(condition, errorKey);
+		}
+
+		public static ICondition<TValue> Satisfies<TValue>(this TValue self, Func<TValue, Task<bool>> condition, string errorKey)
+		{
+			return new PropertyCondition<TValue>(value => Task.Run(async () => await condition(value)).Result, errorKey);
 		}
 
 		public static ChildValidationCondition<TValue> Satisfies<TValue>(this TValue self, IValidator<TValue> validator)
