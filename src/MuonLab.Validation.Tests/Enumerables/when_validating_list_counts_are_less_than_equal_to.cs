23a9d8c46240bee79a83ea5342f20206dfb25c4e
Matt Holland <matt.holland@myunidays.com>
2018-12-05 14:30:25 +0000

    Adds GreaterThan and LessThan extensions to IEnumerables

---
+++ src/MuonLab.Validation.Tests/Enumerables/when_validating_list_counts_are_less_than_equal_to.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace MuonLab.Validation.Tests.Enumerables
+{
+	public class when_validating_list_counts_are_less_than_equal_to
+	{
+		[Fact]
+		public void list_counts_less_than_the_specified_count_should_be_true()
+		{
+			var testClass = new TestClass();
+			testClass.List.Add("foo");
+
+			var validator = new LessThanOrEqualToValidator();
+
+			var report = validator.Validate(testClass);
+			report.IsValid.ShouldBeTrue();
+		}
+
+		[Fact]
+		public void list_counts_equal_to_the_specified_count_should_be_true()
+		{
+			var testClass = new TestClass();
+			testClass.List.Add("foo");
+			testClass.List.Add("bar");
+
+			var validator = new LessThanOrEqualToValidator();
+
+			var report = validator.Validate(testClass);
+			report.IsValid.ShouldBeTrue();
+		}
+
+		[Fact]
+		public void list_counts_greater_than_the_specified_count_should_be_false()
+		{
+			var testClass = new TestClass();
+			testClass.List.Add("foo");
+			testClass.List.Add("bar");
+			testClass.List.Add("baz");
+
+			var validator = new LessThanOrEqualToValidator();
+
+			var report = validator.Validate(testClass);
+			report.IsValid.ShouldBeFalse();
+		}
+
+		[Fact]
+		public void list_counts_less_than_the_specified_predicate_count_should_be_true()
+		{
+			var testClass = new TestClass();
+			testClass.List.Add("foo");
+			testClass.List.Add("bar");
+
+			var validator = new LessThanOrEqualToWithPredicateValidator();
+
+			var report = validator.Validate(testClass);
+			report.IsValid.ShouldBeTrue();
+		}
+
+		[Fact]
+		public void list_counts_equal_to_the_specified_predicate_count_should_be_true()
+		{
+			var testClass = new TestClass();
+			testClass.List.Add("foo");
+			testClass.List.Add("bar");
+			testClass.List.Add("baz");
+
+			var validator = new LessThanOrEqualToWithPredicateValidator();
+
+			var report = validator.Validate(testClass);
+			report.IsValid.ShouldBeTrue();
+		}
+
+		[Fact]
+		public void list_counts_greater_than_the_specified_predicate_count_should_be_false()
+		{
+			var testClass = new TestClass();
+			testClass.List.Add("foo");
+			testClass.List.Add("bar");
+			testClass.List.Add("baz");
+			testClass.List.Add("buux");
+
+			var validator = new LessThanOrEqualToWithPredicateValidator();
+
+			var report = validator.Validate(testClass);
+			report.IsValid.ShouldBeFalse();
+		}
+
+
+		public class TestClass
+		{
+			public IList<string> List { get; set; } = new List<string>();
+		}
+
+		public class LessThanOrEqualToValidator : Validator<TestClass>
+		{
+			protected override void Rules()
+			{
+				this.Ensure(x => x.List.HasCountLessThanOrEqualTo(2));
+			}
+		}
+
+		public class LessThanOrEqualToWithPredicateValidator : Validator<TestClass>
+		{
+			protected override void Rules()
+			{
+				this.Ensure(x => x.List.HasCountLessThanOrEqualTo(y => y.StartsWith("b"), 2));
+			}
+		}
+	}
+}
--- src/MuonLab.Validation/Extensions/IEnumerableExtensions.cs
+++ src/MuonLab.Validation/Extensions/IEnumerableExtensions.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 // ReSharper disable once CheckNamespace
 namespace MuonLab.Validation
@@ -12,7 +15,78 @@ namespace MuonLab.Validation
 
 		public static ICondition<IEnumerable> ContainsElements(this IEnumerable self, string errorKey)
 		{
-			return self.Satisfies(x => (x ?? new object[] {}).GetEnumerator().MoveNext(), errorKey);
+			return self.Satisfies(x => (x ?? new object[] { }).GetEnumerator().MoveNext(), errorKey);
+		}
+
+		public static ICondition<IEnumerable<TValue>> HasCountGreaterThan<TValue>(this IEnumerable<TValue> self,
+			int count)
+		{
+			return self.HasCountGreaterThan(x => true, count);
+		}
+
+		public static ICondition<IEnumerable<TValue>> HasCountGreaterThan<TValue>(this IEnumerable<TValue> self,
+			Func<TValue, bool> predicate, int count)
+		{
+			return self.HasCountGreaterThan(predicate, count, "GreaterThan");
+		}
+
+		public static ICondition<IEnumerable<TValue>> HasCountGreaterThan<TValue>(this IEnumerable<TValue> self,
+			Func<TValue, bool> predicate, int count, string errorKey)
+		{
+			return self.Satisfies(x => x.Count(predicate) > count, errorKey);
+		}
+
+		public static ICondition<IEnumerable<TValue>> HasCountGreaterThanOrEqualTo<TValue>(
+			this IEnumerable<TValue> self, int count)
+		{
+			return self.HasCountGreaterThanOrEqualTo(x => true, count);
+		}
+
+		public static ICondition<IEnumerable<TValue>> HasCountGreaterThanOrEqualTo<TValue>(
+			this IEnumerable<TValue> self, Func<TValue, bool> predicate, int count)
+		{
+			return self.HasCountGreaterThanOrEqualTo(predicate, count, "GreaterThanEq");
+		}
+
+		public static ICondition<IEnumerable<TValue>> HasCountGreaterThanOrEqualTo<TValue>(
+			this IEnumerable<TValue> self, Func<TValue, bool> predicate, int count, string errorKey)
+		{
+			return self.Satisfies(x => x.Count(predicate) >= count, errorKey);
+		}
+
+		public static ICondition<IEnumerable<TValue>> HasCountLessThan<TValue>(this IEnumerable<TValue> self, int count)
+		{
+			return self.HasCountLessThan(x => true, count);
+		}
+
+		public static ICondition<IEnumerable<TValue>> HasCountLessThan<TValue>(this IEnumerable<TValue> self,
+			Func<TValue, bool> predicate, int count)
+		{
+			return self.HasCountLessThan(predicate, count, "LessThan");
+		}
+
+		public static ICondition<IEnumerable<TValue>> HasCountLessThan<TValue>(this IEnumerable<TValue> self,
+			Func<TValue, bool> predicate, int count, string errorKey)
+		{
+			return self.Satisfies(x => x.Count(predicate) < count, errorKey);
+		}
+
+		public static ICondition<IEnumerable<TValue>> HasCountLessThanOrEqualTo<TValue>(this IEnumerable<TValue> self,
+			int count)
+		{
+			return self.HasCountLessThanOrEqualTo(x => true, count);
+		}
+
+		public static ICondition<IEnumerable<TValue>> HasCountLessThanOrEqualTo<TValue>(this IEnumerable<TValue> self,
+			Func<TValue, bool> predicate, int count)
+		{
+			return self.HasCountLessThanOrEqualTo(predicate, count, "LessThanEq");
+		}
+
+		public static ICondition<IEnumerable<TValue>> HasCountLessThanOrEqualTo<TValue>(this IEnumerable<TValue> self,
+			Func<TValue, bool> predicate, int count, string errorKey)
+		{
+			return self.Satisfies(x => x.Count(predicate) <= count, errorKey);
 		}
 	}
 }
