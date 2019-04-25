4ef9d6f6be0e13f3965106e668758b642a581997
Andrew Bullock <trullock@gmail.com>
2019-04-25 09:18:58 +0100

    added missing validation extensions

--- src/MuonLab.Validation/Extensions/IEnumerableExtensions.cs
+++ src/MuonLab.Validation/Extensions/IEnumerableExtensions.cs
@@ -18,73 +18,84 @@ namespace MuonLab.Validation
 			return self.Satisfies(x => (x ?? new object[] { }).GetEnumerator().MoveNext(), errorKey);
 		}
 
-		public static ICondition<IEnumerable<TValue>> HasCountGreaterThan<TValue>(this IEnumerable<TValue> self,
-			int count)
+
+		public static ICondition<IEnumerable<TValue>> HasCountGreaterThan<TValue>(this IEnumerable<TValue> self, int count)
 		{
-			return self.HasCountGreaterThan(x => true, count);
+			return self.HasCountGreaterThan(count, "GreaterThanCount");
 		}
 
-		public static ICondition<IEnumerable<TValue>> HasCountGreaterThan<TValue>(this IEnumerable<TValue> self,
-			Func<TValue, bool> predicate, int count)
+		public static ICondition<IEnumerable<TValue>> HasCountGreaterThan<TValue>(this IEnumerable<TValue> self, int count, string errorKey)
+		{
+			return self.HasCountGreaterThan(x => true, count, errorKey);
+		}
+
+		public static ICondition<IEnumerable<TValue>> HasCountGreaterThan<TValue>(this IEnumerable<TValue> self, Func<TValue, bool> predicate, int count)
 		{
 			return self.HasCountGreaterThan(predicate, count, "GreaterThanCount");
 		}
 
-		public static ICondition<IEnumerable<TValue>> HasCountGreaterThan<TValue>(this IEnumerable<TValue> self,
-			Func<TValue, bool> predicate, int count, string errorKey)
+		public static ICondition<IEnumerable<TValue>> HasCountGreaterThan<TValue>(this IEnumerable<TValue> self, Func<TValue, bool> predicate, int count, string errorKey)
 		{
 			return self.Satisfies(x => x.Count(predicate) > count, errorKey);
 		}
 
-		public static ICondition<IEnumerable<TValue>> HasCountGreaterThanOrEqualTo<TValue>(
-			this IEnumerable<TValue> self, int count)
+
+		public static ICondition<IEnumerable<TValue>> HasCountGreaterThanOrEqualTo<TValue>(this IEnumerable<TValue> self, int count)
 		{
-			return self.HasCountGreaterThanOrEqualTo(x => true, count);
+			return self.HasCountGreaterThanOrEqualTo(count, "GreaterThanEqCount");
+		}
+		public static ICondition<IEnumerable<TValue>> HasCountGreaterThanOrEqualTo<TValue>(this IEnumerable<TValue> self, int count, string errorKey)
+		{
+			return self.HasCountGreaterThanOrEqualTo(x => true, count, errorKey);
 		}
 
-		public static ICondition<IEnumerable<TValue>> HasCountGreaterThanOrEqualTo<TValue>(
-			this IEnumerable<TValue> self, Func<TValue, bool> predicate, int count)
+		public static ICondition<IEnumerable<TValue>> HasCountGreaterThanOrEqualTo<TValue>(this IEnumerable<TValue> self, Func<TValue, bool> predicate, int count)
 		{
 			return self.HasCountGreaterThanOrEqualTo(predicate, count, "GreaterThanEqCount");
 		}
 
-		public static ICondition<IEnumerable<TValue>> HasCountGreaterThanOrEqualTo<TValue>(
-			this IEnumerable<TValue> self, Func<TValue, bool> predicate, int count, string errorKey)
+		public static ICondition<IEnumerable<TValue>> HasCountGreaterThanOrEqualTo<TValue>(this IEnumerable<TValue> self, Func<TValue, bool> predicate, int count, string errorKey)
 		{
 			return self.Satisfies(x => x.Count(predicate) >= count, errorKey);
 		}
 
+
 		public static ICondition<IEnumerable<TValue>> HasCountLessThan<TValue>(this IEnumerable<TValue> self, int count)
 		{
-			return self.HasCountLessThan(x => true, count);
+			return self.HasCountLessThan(count, "LessThanCount");
 		}
 
-		public static ICondition<IEnumerable<TValue>> HasCountLessThan<TValue>(this IEnumerable<TValue> self,
-			Func<TValue, bool> predicate, int count)
+		public static ICondition<IEnumerable<TValue>> HasCountLessThan<TValue>(this IEnumerable<TValue> self, int count, string errorKey)
+		{
+			return self.HasCountLessThan(x => true, count, errorKey);
+		}
+
+		public static ICondition<IEnumerable<TValue>> HasCountLessThan<TValue>(this IEnumerable<TValue> self, Func<TValue, bool> predicate, int count)
 		{
 			return self.HasCountLessThan(predicate, count, "LessThanCount");
 		}
 
-		public static ICondition<IEnumerable<TValue>> HasCountLessThan<TValue>(this IEnumerable<TValue> self,
-			Func<TValue, bool> predicate, int count, string errorKey)
+		public static ICondition<IEnumerable<TValue>> HasCountLessThan<TValue>(this IEnumerable<TValue> self, Func<TValue, bool> predicate, int count, string errorKey)
 		{
 			return self.Satisfies(x => x.Count(predicate) < count, errorKey);
 		}
 
-		public static ICondition<IEnumerable<TValue>> HasCountLessThanOrEqualTo<TValue>(this IEnumerable<TValue> self,
-			int count)
+
+		public static ICondition<IEnumerable<TValue>> HasCountLessThanOrEqualTo<TValue>(this IEnumerable<TValue> self, int count)
 		{
-			return self.HasCountLessThanOrEqualTo(x => true, count);
+			return self.HasCountLessThanOrEqualTo(count, "LessThanEqCount");
+		}
+		public static ICondition<IEnumerable<TValue>> HasCountLessThanOrEqualTo<TValue>(this IEnumerable<TValue> self, int count, string errorKey)
+		{
+			return self.HasCountLessThanOrEqualTo(x => true, count, errorKey);
 		}
 
-		public static ICondition<IEnumerable<TValue>> HasCountLessThanOrEqualTo<TValue>(this IEnumerable<TValue> self,
-			Func<TValue, bool> predicate, int count)
+		public static ICondition<IEnumerable<TValue>> HasCountLessThanOrEqualTo<TValue>(this IEnumerable<TValue> self, Func<TValue, bool> predicate, int count)
 		{
 			return self.HasCountLessThanOrEqualTo(predicate, count, "LessThanEqCount");
 		}
 
-		public static ICondition<IEnumerable<TValue>> HasCountLessThanOrEqualTo<TValue>(this IEnumerable<TValue> self,
-			Func<TValue, bool> predicate, int count, string errorKey)
+		public static ICondition<IEnumerable<TValue>> HasCountLessThanOrEqualTo<TValue>(this IEnumerable<TValue> self, Func<TValue, bool> predicate, int count, string errorKey)
 		{
 			return self.Satisfies(x => x.Count(predicate) <= count, errorKey);
 		}
