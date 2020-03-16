53ad8bdf421eea2849e1a48bee49e7210e5132c4
Andrew Bullock <trullock@gmail.com>
2020-03-16 23:23:26 +0000

    added enum validation error key overload

--- src/MuonLab.Validation.Tests/Enums/EnumExtensions.cs
+++ src/MuonLab.Validation.Tests/Enums/EnumExtensions.cs
@@ -5,9 +5,27 @@ namespace MuonLab.Validation.Tests.Enums
 {
 	public static class EnumExtensions
 	{
+		/// <summary>
+		/// Ensures the value is a valid (declared) Enum value
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="self"></param>
+		/// <returns></returns>
 		public static ICondition<T> IsAValidEnumValue<T>(this T self) where T : Enum
 		{
-			return self.Satisfies(e => Enum.GetValues(self.GetType()).Cast<Enum>().Contains(self), "Invalid");
+			return self.IsAValidEnumValue("Invalid");
+		}
+
+		/// <summary>
+		/// Ensures the value is a valid (declared) Enum value
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="self"></param>
+		/// <param name="errorKey"></param>
+		/// <returns></returns>
+		public static ICondition<T> IsAValidEnumValue<T>(this T self, string errorKey) where T : Enum
+		{
+			return self.Satisfies(e => Enum.GetValues(self.GetType()).Cast<Enum>().Contains(self), errorKey);
 		}
 	}
 }
