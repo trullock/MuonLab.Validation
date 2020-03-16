82a88b60cbd8c0d5004e8c60bef17805126b60cf
Andrew Bullock <trullock@gmail.com>
2020-03-16 20:57:43 +0000

    added enum validation clause

---
+++ src/MuonLab.Validation.Tests/Enums/EnumExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace MuonLab.Validation.Tests.Enums
+{
+	public static class EnumExtensions
+	{
+		public static ICondition<T> IsAValidEnumValue<T>(this T self) where T : Enum
+		{
+			return self.Satisfies(e => Enum.GetValues(self.GetType()).Cast<Enum>().Contains(self), "Invalid");
+		}
+	}
+}
---
+++ src/MuonLab.Validation.Tests/Enums/When_validating_a_property_as_a_valid_enum.cs
@@ -0,0 +1,54 @@
+using System.Linq;
+using Xunit;
+
+namespace MuonLab.Validation.Tests.Enums
+{
+	public class When_validating_a_property_as_a_valid_enum
+	{
+		[Fact]
+		public void ensure_false_returns_true()
+		{
+			var validator = new TestClassValidator();
+			var testClass = new TestClass(TestClass.MyEnum.One);
+
+			var validationReport = validator.Validate(testClass);
+
+			validationReport.IsValid.ShouldBeTrue();
+		}
+
+		[Fact]
+		public void ensure_true_returns_false()
+		{
+			var validator = new TestClassValidator();
+			var testClass = new TestClass((TestClass.MyEnum)5);
+
+			var validationReport = validator.Validate(testClass);
+
+			validationReport.Violations.First().Error.Key.ShouldEqual("Invalid");
+		}
+
+		private class TestClass
+		{
+			public MyEnum enumm { get; set; }
+
+			public TestClass(MyEnum enumm)
+			{
+				this.enumm = enumm;
+			}
+
+			public enum MyEnum
+			{
+				One = 1,
+				Two = 2
+			}
+		}
+
+		private class TestClassValidator : Validator<TestClass>
+		{
+			protected override void Rules()
+			{
+				Ensure(x => x.enumm.IsAValidEnumValue());
+			}
+		}
+	}
+}
